47bf77346365ba1b825c3589ddbc2abb9e7ca077
David Richardson <02richardsondavid@gmail.com>
2024-12-17 00:20:46 -0500

    Initial Commit

---
+++ ECE-370_Standardized_CSV_Data_Analysis/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ECE-370_Standardized_CSV_Data_Analysis
+//
+//  Created by 98dav on 12/17/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
